bb04c6214a4a5490f61694ac5361d288ba4fc720
keanlee <haoli201410@163.com>
2017-05-15 17:55:48 +0800

    mysql configuration

--- deploy-openstack/modularization/controller.sh
+++ deploy-openstack/modularization/controller.sh
@@ -1,28 +1,73 @@
 #!/bin/bash
 
-function install_packages_controller(){
+function packages_of_controller(){
 #-----------------install all of package of controller node ------------------------------------
-    yum install openstack-selinux python-openstackclient -y 1>/dev/null 
-    debug "$?" "$RED yum install openstack-selinux python-openstackclient failed $NO_COLOR"
-    yum install openstack-keystone httpd mod_wsgi  memcached python-memcached -y 1>/dev/null
-    debug "$?" "$RED yum install openstack-keystone failed $NO_COLOR"
-    yum install openstack-glance  -y  1>/dev/null
-    debug "$?" "$RED yum install openstack-glance failed $NO_COLOR"
-    yum install openstack-nova-api openstack-nova-cert openstack-nova-conductor openstack-nova-console openstack-nova-novncproxy openstack-nova-scheduler -y 1>/dev/null
-    debug  "$?" "$RED yum install openstack-nova-api failed $NO_COLOR"
-    yum install openstack-neutron openstack-neutron-ml2 openstack-neutron-openvswitch -y 1>/dev/null
-    debug  "$?" "$RED yum install openstack-neutron failed $NO_COLOR"
-    yum install openstack-dashboard  -y 1>/dev/null
-    debug  "$?" "$RED yum install openstack-dashboard failed $NO_COLOR"
-    yum install openstack-cinder  -y 1>/dev/null
-    debug  "$?" "$RED yum install openstack-cinder failed $NO_COLOR"
-    yum install openstack-ceilometer-api openstack-ceilometer-collector openstack-ceilometer-notification openstack-ceilometer-central python-ceilometerclient -y 1>/dev/null
-    debug  "$?" "yum install ceilometer failed $NO_COLOR"
-    yum install python-rbd -y 1>/dev/null
-    debug  "$?" "yum install python-rbd failed $NO_COLOR "
+echo $BLUE Beginning install packages of controller node On $(hostname) , please be wait ... $NO_COLOR 
+ yum install mariadb mariadb-server python2-PyMySQL -y 1>/dev/null 
+    debug "$1" "$RED Install mariadb mariadb-server python2-PyMySQL failed $NO_COLOR"
+    
+ yum install openstack-selinux python-openstackclient -y 1>/dev/null 
+    debug "$?" "$RED Install openstack-selinux python-openstackclient failed $NO_COLOR"
+    
+ yum install openstack-keystone httpd mod_wsgi  memcached python-memcached -y 1>/dev/null
+    debug "$?" "$RED Install openstack-keystone failed $NO_COLOR"
+    
+ yum install openstack-glance  -y  1>/dev/null
+    debug "$?" "$RED Install openstack-glance failed $NO_COLOR"
+    
+ yum install openstack-nova-api openstack-nova-cert openstack-nova-conductor openstack-nova-console openstack-nova-novncproxy openstack-nova-scheduler -y 1>/dev/null
+    debug  "$?" "$RED Install openstack-nova-api failed $NO_COLOR"
+    
+ yum install openstack-neutron openstack-neutron-ml2 openstack-neutron-openvswitch -y 1>/dev/null
+    debug  "$?" "$RED Install openstack-neutron failed $NO_COLOR"
+    
+ yum install openstack-dashboard  -y 1>/dev/null
+    debug  "$?" "$RED Install openstack-dashboard failed $NO_COLOR"
+   
+ yum install openstack-cinder  -y 1>/dev/null
+    debug  "$?" "$RED Install openstack-cinder failed $NO_COLOR"
+   
+ yum install openstack-ceilometer-api openstack-ceilometer-collector openstack-ceilometer-notification openstack-ceilometer-central python-ceilometerclient -y 1>/dev/null
+    debug  "$?" "$RED Install ceilometer failed $NO_COLOR"
+   
+ yum install python-rbd -y 1>/dev/null
+    debug  "$?" "$RED Install python-rbd failed $NO_COLOR "
 
+echo $GREEN Finshed install all packages of controller on $YELLOW $(hostname) $NO_COLOR 
 }
 
+function mysql_configuration(){
+echo $BLUE Beginning configuration mysql ,keystone ,memcache,glance,nova for controller node on $(hostname) $NO_COLOR
+# set the bind-address key to the management IP address of the controller node to enable access by other nodes via the management network
+# refer https://docs.openstack.org/newton/install-guide-rdo/environment-sql-database.html
+    echo > /etc/my.cnf.d/openstack.cnf
+    cat > /etc/my.cnf.d/openstack.cnf <<EOF
+[mysqld]
+bind-address = $MGMT_IP
+default-storage-engine = innodb
+innodb_file_per_table
+max_connections=5096
+collation-server = utf8_general_ci
+character-set-server = utf8
+init-connect = 'SET NAMES utf8'
+EOF
+systemctl enable mariadb.service 1>/dev/null && 
+systemctl start mariadb.service
+#sed -i '/Group=mysql/a\LimitNOFILE=65535' /usr/lib/systemd/system/mariadb.service
+#systemctl daemon-reload
+systemctl restart mariadb.service
 
+mysql_secure_installation <<EOF
+
+y
+$MARIADB_PASSWORD
+$MARIADB_PASSWORD
+y
+y
+y
+y
+EOF
+debug "$?" "$RED Mysql configuration failed $NO_COLOR"
+}
 
 
