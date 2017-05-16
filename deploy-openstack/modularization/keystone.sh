a7ac027f24cc5cd47f45a196bdd06a6771e9b245
Kean.Lee <haoli201410@163.com>
2017-05-16 17:29:51 +0800

    Update keystone.sh
    remove functions mysql_configuration and database_create function

--- deploy-openstack/modularization/keystone.sh
+++ deploy-openstack/modularization/keystone.sh
@@ -4,43 +4,6 @@
 yum install openstack-selinux python-openstackclient -y 1>/dev/null 
 debug "$?" "$RED Install openstack-selinux python-openstackclient failed $NO_COLOR"
 
-function mysql_configuration(){
-echo $BLUE Beginning configuration mysql for controller node on $YELLOW $(hostname) $NO_COLOR
-# set the bind-address key to the management IP address of the controller node to enable access by other nodes via the management network
-# refer https://docs.openstack.org/newton/install-guide-rdo/environment-sql-database.html
-yum install mariadb mariadb-server python2-PyMySQL -y 1>/dev/null 
-debug "$1" "$RED Install mariadb mariadb-server python2-PyMySQL failed $NO_COLOR"   
-    echo > /etc/my.cnf.d/openstack.cnf
-    cat > /etc/my.cnf.d/openstack.cnf <<EOF
-[mysqld]
-bind-address = $MGMT_IP
-default-storage-engine = innodb
-innodb_file_per_table
-max_connections=4096
-collation-server = utf8_general_ci
-character-set-server = utf8
-init-connect = 'SET NAMES utf8'
-EOF
-systemctl enable mariadb.service 1>/dev/null && 
-systemctl start mariadb.service
-#sed -i '/Group=mysql/a\LimitNOFILE=65535' /usr/lib/systemd/system/mariadb.service
-#systemctl daemon-reload
-systemctl restart mariadb.service
-
-mysql_secure_installation <<EOF
-
-y
-$MARIADB_PASSWORD
-$MARIADB_PASSWORD
-y
-y
-y
-y
-EOF
-debug "$?" "$RED Mysql configuration failed $NO_COLOR"
-echo $GREEN Finished the Mariadb install and configuration on $YELLOW $(hostname) $NO_COLOR 
-}
-
 function rabbitmq_configuration(){
 #RABBIT_P 
 #Except Horizone and keystone ,each component need connect to Rabbitmq 
@@ -68,16 +31,6 @@ yum install memcached python-memcached -y 1>/dev/null
 sed -i "s/127.0.0.1/$MGMT_IP/g" /etc/sysconfig/memcached
 systemctl enable memcached.service && 1>/dev/null
 systemctl start memcached.service
-}
-
-function database_create(){
-#create database and user in mariadb for openstack component
-#$1 is the database name (comonent name and usename) 
-#$2 is password of database
-
-mysql -uroot -p$MARIADB_PASSWORD -e "create database $1 character set utf8;grant all privileges on $1.* to $1@localhost \
-identified by '$2';flush privileges;"  
-debug "$?" "Create database $1 Failed "
 }
 
 function keystone(){
