25f1e0657c32da267c85cb0e210d4b9e9b3952b2
Kean.Lee <haoli201410@163.com>
2017-05-17 15:24:24 +0800

    Update keystone.sh

--- deploy-openstack/bin/keystone.sh
+++ deploy-openstack/bin/keystone.sh
@@ -154,7 +154,9 @@ echo $GREEN Beginning install $YELLOW KEYSTONE $NO_COLOR $GREEN ... $NO_COLOR
 #Execute below function to install keystone 
 yum install openstack-selinux python-openstackclient -y 1>/dev/null 
 debug "$?" "$RED Install openstack-selinux python-openstackclient failed $NO_COLOR"
+#------Function ------
 ntp
+mysql_configuration
 rabbitmq_configuration
 memcache
 keystone
