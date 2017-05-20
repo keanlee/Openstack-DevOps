bc4a941abd69c8c73ff239b1fd2308c5369c394a
Kean.Lee <haoli201410@163.com>
2017-05-20 08:57:26 +0800

    Update install.sh
    remove color and debug function to common.sh

--- deploy-openstack/install.sh
+++ deploy-openstack/install.sh
@@ -2,19 +2,7 @@
 #author by keanlee on May 15th of 2017 
 
 cd $(cd $(dirname $0); pwd)
-# ansi colors for formatting heredoc
-ESC=$(printf "\e")
-GREEN="$ESC[0;32m"
-NO_COLOR="$ESC[0;0m"
-RED="$ESC[0;31m"
-MAGENTA="$ESC[0;35m"
-YELLOW="$ESC[0;33m"
-BLUE="$ESC[0;34m"
-WHITE="$ESC[0;37m"
-#PURPLE="$ESC[0;35m"
-CYAN="$ESC[0;36m"
 
-source ./bin/VARIABLE 
 
 README=$(cat ./README.txt)
 echo $GREEN $README $NO_COLOR 
@@ -33,12 +21,6 @@ help
 exit 1
 fi
 
-debug(){
-if [[ $1 -ne 0 ]]; then 
-echo $RED $2 $NO_COLOR
-exit 1
-fi
-}
 
 #---------------keystone -----------
 case $1 in
