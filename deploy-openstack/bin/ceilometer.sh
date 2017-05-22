687009d3778c7d8a07f038e383c797659c96993a
keanlee <haoli201410@163.com>
2017-05-22 15:04:55 +0800

    add ceilometer script,will finish it later

---
+++ deploy-openstack/bin/ceilometer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#The Telemetry Data Collection services provide the following functions:
+
+#Efficiently polls metering data related to OpenStack services.
+#Collects event and metering data by monitoring notifications sent from services.
+#Publishes collected data to various targets including data stores and message queues
+
+function cell
