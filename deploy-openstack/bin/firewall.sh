3e0ba8de1d9a7cd2c0b9924b69dae45c5d00cadc
keanlee <haoli201410@163.com>
2017-06-09 17:25:29 +0800

    add fireall ruler list

--- deploy-openstack/bin/firewall.sh
+++ deploy-openstack/bin/firewall.sh
@@ -6,30 +6,25 @@
 function iptabels(){
 iptables -I  INPUT -p tcp --dport 22    -j ACCEPT
 iptables -A  INPUT -p tcp --dport 80    -j ACCEPT
-
 #add rabbitmq port 
 iptables -A  INPUT -p tcp --dport 5672 -j ACCEPT
 #add rabbitmq web plugin port 
 iptables -A  INPUT -p tcp --dport 15672 -j ACCEPT
-
 #----------neutron-----------------------
 iptables -A  INPUT -p tcp --dport 9696 -j ACCEPT
-
 #----------keystone--------------------
 iptables -A  INPUT -p tcp --dport 35357 -j ACCEPT
 iptables -A  INPUT -p tcp --dport 5000 -j ACCEPT
-
 #----------cinder---------------------
 #cinderv2
 iptables -A  INPUT -p tcp --dport 8776 -j ACCEPT
-
 #------------glance---------------------
 iptables -A  INPUT -p tcp --dport 9292 -j ACCEPT
-
-
 #--------------nova--------------------
 iptables -A  INPUT -p tcp --dport 8774 -j ACCEPT
-
+#-------------zabbix agent -------------------
+iptables -A  INPUT -p tcp --dport 10050 -j ACCEPT
+iptables -A  INPUT -p tcp --dport 10051 -j ACCEPT
 iptables-save > /etc/sysconfig/iptables
 }
 
@@ -38,10 +33,16 @@ firewall-cmd --zone=public --add-port=80/tcp --permanent  1>/dev/null 2>&1
 firewall-cmd --zone=public --add-port=22/tcp --permanent  1>/dev/null 2>&1
 firewall-cmd --zone=public --add-port=10050/tcp --permanent 1>/dev/null 2>&1
 firewall-cmd --zone=public --add-port=10051/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=5672/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=15672/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=9696/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=35357/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=5000/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=8776/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=9292/tcp --permanent 1>/dev/null 2>&1
+firewall-cmd --zone=public --add-port=8774/tcp --permanent 1>/dev/null 2>&1
 firewall-cmd --reload  1>/dev/null 2>&1
 }
-
-
 
 #for now on 
 echo $BLUE Disable the firewall $NO_COLOR
