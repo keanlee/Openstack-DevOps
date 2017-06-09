5670c47cd4a72ea5cd4291b20bc741c0f0a5f47f
keanlee <haoli201410@163.com>
2017-06-09 11:59:19 +0800

    add comments for conf file

--- deploy-openstack/bin/neutron.sh
+++ deploy-openstack/bin/neutron.sh
@@ -99,7 +99,6 @@ __EOF__
 
 echo $BLUE To configure prerequisites $NO_COLOR
 cat >> /etc/sysctl.conf << __EOF__
-net.ipv4.ip_forward = 1
 net.ipv4.conf.all.rp_filter = 0
 net.ipv4.conf.default.rp_filter = 0
 __EOF__
