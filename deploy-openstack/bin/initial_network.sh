2b2620bfeaac5ec08774da0e801a8f98fc8d9021
keanlee <haoli201410@163.com>
2017-06-12 14:27:30 +0800

    add flating ip create script

--- deploy-openstack/bin/initial_network.sh
+++ deploy-openstack/bin/initial_network.sh
@@ -6,10 +6,17 @@
 
 source /home/admin-openrc
 
+
+#disable ip address of devcie 
+
+#ifconfig eth0 0
+
+#ip addr del dev eth0 1.1.1.1/24
+
+
 echo $BLUE Create external network $NO_COLOR 
 #---------------- the network name ---
-neutron net-create ext-net --router:external \   
---provider:physical_network external --provider:network_type vxlan 
+neutron net-create external-net --router:external=True
 
 #Like a physical network, a virtual network requires a subnet assigned to it. 
 #The external net-work shares the same subnet and gateway associated with the physical network 
@@ -17,10 +24,19 @@ neutron net-create ext-net --router:external \
 #thissubnet for router and floating IP addresses 
 #to prevent interference with other devices onthe external network
 
-echo $BLUE Create the subnet $NO_COLOR
+echo $BLUE Create the subnet for external network  $NO_COLOR
 neutron subnet-create ext-net $EXTERNAL_NETWORK_CIDR --name ext-subnet \ 
 --allocation-pool start=$FLOATING_IP_START,end=$FLOATING_IP_END \  
 --disable-dhcp --gateway $EXTERNAL_NETWORK_GATEWAY
 
+echo $BLUE Create a router $NO_COLOR 
+neutron router-create pub-router
+
+echo $BLUE Let the router connect the external network $NO_COLOR 
+neutron router-gateway-set pub-router external-net
 
 
+#For example 
+#neutron subnet-create external-net 10.245.58.0/24 --name external_subnet \
+#--enable_dhcp=False --allocation-pool start=10.245.58.2,end=10.245.58.20 \
+#--gateway=10.245.58.1
