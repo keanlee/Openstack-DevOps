7169f3397f2853b33d87d2d37e093c5aa27d41b4
keanlee <haoli201410@163.com>
2017-09-04 21:25:30 -0400

    add haproxy health check script

---
+++ deploy-openstack/etc/HA/haproxy-health-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+HA_PROXY_STATUS=$(ps -C haproxy --no-header | wc -l )
+
+if [[  $HA_PROXY_STATUS -eq 0 ]];then 
+    systemctl start haproxy
+    sleep 4
+    if [[ $HA_PROXY_STATUS -eq 0  ]];then 
+        systemctl stop keepalived
+    fi
+fi 
+
